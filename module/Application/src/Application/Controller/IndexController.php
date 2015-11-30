cbdc70216b522e6f7c52ce54ce77d94cf59c7861
Nikolay Kovenko <nickk@templatemonster.me>
2015-11-30 10:56:42 +0200

    Добавлен скелет приложения

---
+++ module/Application/src/Application/Controller/IndexController.php
@@ -0,0 +1,24 @@
+<?php
+namespace Application\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+/**
+ * Class IndexController
+ * @package Application\Controller
+ */
+class IndexController extends AbstractActionController
+{
+
+    /**
+     * Главная страница приложения
+     * @return ViewModel
+     */
+    public function indexAction()
+    {
+        return new ViewModel([
+            ]
+        );
+    }
+}
