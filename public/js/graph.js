bad1b356ce7749251363513211bed9cd9d304f64
Nikolay Kovenko <nickk@templatemonster.me>
2016-01-25 15:40:11 +0200

    Рефакторинг JS

--- public/js/graph.js
+++ public/js/graph.js
@@ -1,28 +1,56 @@
-$(function() {
+var visualizator = function(data, options) {
 
-    for (var graphType in groupsRaw) {
+    this.options = {};
+    this.options.container = options.container;
+    this.options.groupsOptions = options.groupsOptions || {};
+    this.options.graphOptions = options.graphOptions || {};
 
+    this.visualize = function() {
+        var result = [];
+
+        for (var key in data) {
+
+            var graph = data[key],
+             graphContainer = this.createGraphItem(graph.id, graph.title),
+             groups = this.createGraphGroups(graph.groups),
+             dataset = new vis.DataSet(graph.data),
+             container = document.getElementById(graphContainer);
+
+            var graphObject = new vis.Graph2d(container, dataset, groups, this.options.graphOptions);
+
+            result.push(graphObject);
+        }
+
+        return result;
+    };
+
+    this.createGraphItem = function(id, title) {
+        var sectionId = 'visualization-' + id;
+        this.options.container.append(this.sectionTpl(sectionId, title));
+        return sectionId;
+    };
+
+    this.sectionTpl = function(itemId, title) {
+        return '<section>' +
+            '<h4>' + title + '</h4>' +
+            '<div id="' + itemId + '"></div>' +
+         '</section>';
+    };
+
+    this.createGraphGroups = function(groupsData) {
         var groups = new vis.DataSet();
-        for (var key in groupsRaw[graphType]) {
-            var group = groupsRaw[graphType][key];
+
+        for (var groupKey in groupsData) {
+            var groupName = groupsData[groupKey];
 
             groups.add({
-                id: group,
-                content: group,
-                options: {
-                    drawPoints: {
-                        style: 'circle'
-                    }
-                }
+                id: groupName,
+                content: groupName,
+                options: this.options.groupsOptions
             });
         }
 
-        var dataset = new vis.DataSet(items[graphType]);
-        var options = {
-            legend: true
-        };
-
-        var container = document.getElementById('visualization-' + graphType);
-        var graph2d = new vis.Graph2d(container, dataset, groups, options);
+        return groups;
     }
-});
+};
+
